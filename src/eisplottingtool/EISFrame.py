dcf4f2dcadc7ef1f5c44d551165e00ae144a1530
ileu <usauterv@outlook.ocm>
2022-01-31 23:42:54 +0100

    v0.1.3 Multi Indices

--- src/eisplottingtool/EISFrame.py
+++ src/eisplottingtool/EISFrame.py
@@ -10,9 +10,8 @@ import os
 import re
 from typing import TypeVar
 
-import pandas as pd
 import eclabfiles as ecf
-
+import pandas as pd
 
 LOGGER = logging.getLogger(__name__)
 T = TypeVar('T', bound='Parent')
@@ -20,6 +19,7 @@ T = TypeVar('T', bound='Parent')
 
 def load_df_from_path(path):
     return None
+
 
 def _get_default_data_param(columns):
     col_names = {}
@@ -42,7 +42,7 @@ def _get_default_data_param(columns):
             col_names['cycle'] = match.group()
         elif match := re.match(r'freq[^|]*', col):
             col_names['frequency'] = match.group()
-        elif match := re.match(r'Ns[^|]*', col):
+        elif match := re.match(r'Ns', col):
             col_names['Ns'] = match.group()
     return col_names
 
@@ -137,65 +137,17 @@ class EISFrame:
         col_names = _get_default_data_param(data.columns)
         if col_names.get("cycle") is None:
             raise ValueError(f"No cycles detetected in file {path}.")
+        print(data.columns)
         print(col_names)
-        self.df = data
+        self.df = data.copy()
 
-        data.groupby([col_names["cycle"], col_names.get("Ns")], )
-        for key, values in data.iteritems():
-            print(data.ix[values], "\n\n")
+        data.set_index([col_names["cycle"], col_names.get("Ns")], inplace=True)
+        data.sort_index(inplace=True)
 
-
-if __name__ == "__main__":
-    from eisplottingtool import EISFrame
-
-    path1 = r"G:\Collaborators\Sauter Ulrich\Projects\EIS Tail\Data"
-    file1 = r"\20201204_Rabeb_LLZTO_Batch4_rAcetonitryle-3days_Li300C_3mm_0p7th_PT_C15.mpr"
-
-
-    class EPTfile:
-        def __init__(
-                self,
-                path,
-                name,
-                ignore=False,
-                diameter=0,
-                color=None,
-                thickness=1.0,
-                circuit1=None,
-                circuit2=None,
-                initial_par=None,
-        ):
-            self.ignore = ignore
-            self.name = name
-            self.path = path
-            self.thickness = thickness
-            self.color = color
-            self.circuit1 = circuit1
-            self.circuit2 = circuit2
-            self.initial_par = initial_par
-            self.diameter = diameter
-
-
-    cell1 = EPTfile(
-            file1,
-            "Acetonitryle",
-            color="C7",
-            thickness=0.7,
-            diameter=3,
-            circuit1="R0-p(R1,CPE1)-p(R2,CPE2)-Ws1",
-            initial_par=[1, 1500, 1e-8, 0.9, 500, 1e-6, 0.9, 500, 2],
-    )
-
-    testFrame = EISFrame(
-            name="Acetonitryle",
-            path=path1 + file1,
-            color="C7",
-            circuit1="R0-p(R1,CPE1)-p(R2,CPE2)-Ws1",
-            initial_par=[1.0, 1500.0, 1e-8, 0.9, 500.0, 1e-6, 0.9, 500.0, 2.0],
-    )
-
-    print(file1)
-
-    testFrame.load()
-
-    print(testFrame)
+        print(data.columns)
+        print(data.loc[1])
+        print(data.loc[0, 1])
+        print(data.loc[(0, 1), :])
+        print(data.loc[(0, 1), col_names["frequency"]])
+        print(data.loc[([1, 2, 3], 1), col_names["frequency"]])
+        print(data.loc[(slice(None), 1), col_names["frequency"]])
